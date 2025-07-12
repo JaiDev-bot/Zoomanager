b6df3976888916d794b79cce1f0b3cca6c6760a7
Jaiane <jaianesoares700@gmail.com>
2025-07-12 19:46:36 -0300

    First commit

---
+++ src/test/java/com/example/Zoomanager/ZoomanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Zoomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoomanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
