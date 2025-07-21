b8bf15bb379f3e0bf0c0eb6be758c1f25b50404d
HenriBDev <henribdev@gmail.com>
2025-07-21 16:48:52 -0300

    ✨ feat - Adicionado logging para erros

--- src/main/java/com/example/Zoomanager/controllers/AlertaController.java
+++ src/main/java/com/example/Zoomanager/controllers/AlertaController.java
@@ -4,6 +4,8 @@ import com.example.Zoomanager.dto.AlertaSaveDTO;
 import com.example.Zoomanager.service.InterfaceService.AlertaService;
 import com.example.Zoomanager.swagger.AlertaSwagger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/alerta")
 public class AlertaController implements AlertaSwagger {
+
+    private static Logger logger = LoggerFactory.getLogger(AlertaController.class.getName());
 
     @Autowired
     private AlertaService alertaService;
@@ -26,6 +30,7 @@ public class AlertaController implements AlertaSwagger {
             return ResponseEntity.created(null).body("Alerta adicionado com sucesso!");
         }
         catch (Exception e) {
+            logger.error("Não foi possível adicionar o alerta.", e);
             return ResponseEntity.internalServerError().body("Não foi possível adicionar o alerta.");
         }
     }
--- src/main/java/com/example/Zoomanager/utils/EnumUtils.java
+++ src/main/java/com/example/Zoomanager/utils/EnumUtils.java
@@ -3,7 +3,7 @@ package com.example.Zoomanager.utils;
 public class EnumUtils {
     public static <E extends Enum<E>> E getEnumFromString(Class<E> classEnum, String strValue) {
         for (E enumValue : classEnum.getEnumConstants()) {
-            if (enumValue.name().equals(strValue)) {
+            if (enumValue.name().equalsIgnoreCase(strValue)) {
                 return enumValue;
             }
         }
