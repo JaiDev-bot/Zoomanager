1a09eb4ecdf2aacb5ec13ad93624ce65601fc989
HenriBDev <henribdev@gmail.com>
2025-07-25 20:12:56 -0300

    💥 fix - Adicionando novamente o AlertaService

---
+++ src/main/java/com/example/Zoomanager/service/interfaceService/AlertaService.java
@@ -0,0 +1,8 @@
+package com.example.Zoomanager.service.interfaceService;
+
+import com.example.Zoomanager.dto.AlertaSaveDTO;
+
+public interface AlertaService {
+
+    void addAlerta(AlertaSaveDTO AlertaSaveDto);
+}
--- src/test/java/com/example/Zoomanager/service/ServiceIMPL/AlertaServiceIMPL.java
+++ src/test/java/com/example/Zoomanager/service/ServiceIMPL/AlertaServiceIMPL.java
@@ -6,7 +6,7 @@ import com.example.Zoomanager.entity.Animal;
 import com.example.Zoomanager.enums.TipoAlerta;
 import com.example.Zoomanager.repositories.RepositoryInterface.AlertaRepository;
 import com.example.Zoomanager.repositories.RepositoryInterface.AnimalRepository;
-import com.example.Zoomanager.service.InterfaceService.AlertaService;
+import com.example.Zoomanager.service.interfaceService.AlertaService;
 import com.example.Zoomanager.utils.EnumUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
