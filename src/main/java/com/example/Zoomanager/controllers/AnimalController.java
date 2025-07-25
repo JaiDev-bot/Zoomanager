97d8ebb8728aa3a23548416f2451e8efbdb26b78
Jaiane <jaianesoares700@gmail.com>
2025-07-25 09:26:43 -0300

    refactor(AnimalContoller): 🧬️refatora método post, bem como tratamento de exceção

--- src/main/java/com/example/Zoomanager/controllers/AnimalController.java
+++ src/main/java/com/example/Zoomanager/controllers/AnimalController.java
@@ -6,6 +6,8 @@ import com.example.Zoomanager.service.interfaceService.AnimalService;
 
 import com.example.Zoomanager.swagger.AnimalSwagger;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,13 +21,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class AnimalController implements AnimalSwagger {
 
+    private static Logger logger = LoggerFactory.getLogger(AnimalController.class.getName());
+
     @Autowired
     private AnimalService animalService;
 
     @PostMapping
     public ResponseEntity<String> saveAnimal (@RequestBody AnimalSaveDTO animalSaveDTO){
-        String name = animalService.addAnimal(animalSaveDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body("added " + name);
+
+        try{
+
+            String name = animalService.addAnimal(animalSaveDTO);
+            return ResponseEntity.status(HttpStatus.CREATED).body(name + " Foi adicionado com sucesso!");
+
+        } catch (Exception e) {
+
+            logger.error("Não foi possivel adicionar o animal. " , e);
+            return ResponseEntity.internalServerError().body("Não foi possivel adicionar o animal");
+
+        }
     }
 
 
