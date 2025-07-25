85741f8c6d9321c73e9049a61deb089b23757c4f
Henrique Barbosa <77559930+HenriBDev@users.noreply.github.com>
2025-07-25 20:10:06 -0300

    🐛 fix - Corrigindo pasta duplicada

--- src/main/java/com/example/Zoomanager/service/InterfaceService/AlertaService.java
+++
@@ -1,8 +0,0 @@
-package com.example.Zoomanager.service.interfaceService;
-
-import com.example.Zoomanager.dto.AlertaSaveDTO;
-
-public interface AlertaService {
-
-    void addAlerta(AlertaSaveDTO AlertaSaveDto);
-}
