48a3aeab41a85d3ab38aac6c1aad2d070f4b4398
Jaiane <jaianesoares700@gmail.com>
2025-07-27 19:45:38 -0300

    test(AnimalContoller): 🧪️Teste unitário do método GET

--- src/main/java/com/example/Zoomanager/controllers/AnimalController.java
+++ src/main/java/com/example/Zoomanager/controllers/AnimalController.java
@@ -11,10 +11,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/animal")
@@ -40,6 +39,25 @@ public class AnimalController implements AnimalSwagger {
             return ResponseEntity.internalServerError().body("Não foi possivel adicionar o animal");
 
         }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AnimalSaveDTO>> getAnimals() {
+        try {
+            List<AnimalSaveDTO> animals = animalService.getAllAnimals();
+            if (animals.isEmpty()) {
+                return ResponseEntity.noContent().build();
+            }
+            return ResponseEntity.ok(animals);
+
+        } catch (Exception e) {
+
+
+            logger.error("Não foi possível buscar os animais.", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(null);
+        }
+
     }
 
 
