e4a8bf0d143632584f6c3325342aa670c96405dc
HenriBDev <henribdev@gmail.com>
2025-08-04 00:05:23 -0300

    ♻️ refactor - Inserindo novamente a pasta com inicial minúscula

---
+++ src/main/java/com/example/Zoomanager/repositories/repositoryInterface/TarefaRepository.java
@@ -0,0 +1,10 @@
+package com.example.Zoomanager.repositories.repositoryInterface;
+
+import com.example.Zoomanager.entity.Tarefa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+}
