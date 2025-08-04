5baa24a34b547c3ccefdfafa52b5973f9ea5afc8
HenriBDev <henribdev@gmail.com>
2025-08-04 00:03:05 -0300

    🚧 WIP - Removendo temporariamente pasta de repositórios

--- src/main/java/com/example/Zoomanager/repositories/RepositoryInterface/TarefaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Zoomanager.repositories.repositoryInterface;
-
-import com.example.Zoomanager.entity.Tarefa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
-}
