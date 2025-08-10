296bf36c5c57ee3ee0b1d35222a074f83b6b6993
HenriBDev <henribdev@gmail.com>
2025-08-10 16:11:48 -0300

    ♻️ refactor - Ajustado parâmetro de ID da tarefa para ser PathVariable

--- src/main/java/com/example/Zoomanager/controllers/TarefaController.java
+++ src/main/java/com/example/Zoomanager/controllers/TarefaController.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,8 +44,8 @@ public class TarefaController implements TarefaSwagger {
         }
     }
     
-    @PatchMapping
-    public ResponseEntity<?> iniciarExecucaoTarefa(@RequestBody long tarefaId) {
+    @PatchMapping("/iniciar/{tarefaId}")
+    public ResponseEntity<?> iniciarExecucaoTarefa(@PathVariable long tarefaId) {
 
         try{
             tarefaService.iniciarExecucaoTarefa(tarefaId);
